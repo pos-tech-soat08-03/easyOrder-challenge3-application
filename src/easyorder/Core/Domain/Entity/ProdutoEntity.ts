f2e9067c48a63fda544f9eee9dca8a405a3ab3f5
BrunoMreis <brbrno@hotmail.com>
2024-07-29 19:48:39 -0300

    Corrige campo categoria e nome

--- src/easyorder/Core/Domain/Entity/ProdutoEntity.ts
+++ src/easyorder/Core/Domain/Entity/ProdutoEntity.ts
@@ -1,22 +1,24 @@
 
 export class ProdutoEntity {
     private id: number;
-    private nome: Categoria;
+    private nome: string;
     private descricao: string;
     private preco: number;
+    private categoria: Categoria;
     private imagemURL: string;
 
- constructor(id: number, nome: Categoria, descricao:string, preco:number, imagemURL:string) {
+ constructor(id: number, nome: string, descricao:string, preco:number,categoria: Categoria, imagemURL:string) {
     this.id = id;
     this.nome = nome;
     this.descricao = descricao;
     this.preco = preco;
+    this.categoria = categoria;
     this.imagemURL = imagemURL;
  }
  public getId(): number {
     return this.id;
  }
- public getNome(): Categoria {
+ public getNome(): string {
     return this.nome;
  }
  public getDescricao(): string {
@@ -27,6 +29,9 @@ export class ProdutoEntity {
  }
     public getImagemURL(): string {
     return this.imagemURL;
+ }
+ public getCategoria(): Categoria {
+    return this.categoria;
  }
 
 }
