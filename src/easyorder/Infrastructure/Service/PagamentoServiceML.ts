bb665b2852e2d8666c3e0963802c531c72bf6e74
Marcio Saragiotto <marcio.saragiotto@gmail.com>
2024-10-02 21:42:11 -0300

    feature: 80807618 gateway mock pagamento

--- src/easyorder/Infrastructure/Service/PagamentoServiceML.ts
+++ src/easyorder/Infrastructure/Service/PagamentoServiceML.ts
@@ -1,7 +1,17 @@
+import { TransactionEntity } from "../../Core/Entity/TransactionEntity";
 import { PagamentoServiceInterface } from "../../Core/Interfaces/Services/PagamentoServiceInterface";
 
 export class PagamentoServiceML implements PagamentoServiceInterface {
 
+    async processPayment (transaction: TransactionEntity): Promise <TransactionEntity> {
+        // todo: implement
+        return transaction;
+    }
     
+    async handlePaymentResponse (transaction: TransactionEntity): Promise <TransactionEntity> {
+        // todo: implement
+        return transaction;
+    }
+
     
 }
